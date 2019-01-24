072f13824b1a6c3736977259e99ad1897f475200
Weihao Yan (RIT Student) <wy2751@ad.rit.edu>
2019-01-23 23:59:56 -0500

    GGP_Assignment1

---
+++ DX11Starter/DX11Starter/Mesh.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "DXCore.h"
+#include "SimpleShader.h"
+#include <DirectXMath.h>
+#include "Vertex.h"
+#include <d3d11.h>
+
+class Mesh
+{
+public:
+	Mesh(Vertex* vertices, int numVertices,  int* indices, int numIndices, ID3D11Device* device);
+	~Mesh();
+
+	//returns the pointer to the vertex buffer object
+	ID3D11Buffer* GetVertexBuffer();
+
+	//return the pointer to the index buffer object
+	ID3D11Buffer* GetIndexBuffer();
+
+	//return the number of indices this mesh contains
+	int GetIndexCount();
+
+private:
+	// Buffers to hold actual geometry data
+	ID3D11Buffer* vertexBuffer;
+	ID3D11Buffer* indexBuffer;
+
+	//Specifying how many indices are in the mesh's index buffer
+	int numIndices;
+
+
+
+};
+
