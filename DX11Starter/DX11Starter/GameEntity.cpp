9f9d163379f3fb5d9d3884db8cbb8f1f3011a07e
Weihao Yan (RIT Student) <wy2751@ad.rit.edu>
2019-01-28 16:41:31 -0500

    01/28

---
+++ DX11Starter/DX11Starter/GameEntity.cpp
@@ -0,0 +1,76 @@
+#include "GameEntity.h"
+
+
+
+GameEntity::GameEntity(Mesh* object)
+{
+	gameObj = object;
+
+	//Set default vaules for the matrix
+	translation =
+	{
+		1.0f, 0.0f, 0.0f, 0.0f,
+		0.0f, 1.0f, 0.0f, 0.0f,
+		0.0f, 0.0f, 1.0f, 0.0f,
+		0.0f, 0.0f, 0.0f, 1.0f,
+	};
+
+	rotation =
+	{
+		1.0f, 0.0f, 0.0f, 0.0f,
+		0.0f, 1.0f, 0.0f, 0.0f,
+		0.0f, 0.0f, 1.0f, 0.0f,
+		0.0f, 0.0f, 0.0f, 1.0f,
+	};
+
+	scale =
+	{
+		1.0f, 0.0f, 0.0f, 0.0f,
+		0.0f, 1.0f, 0.0f, 0.0f,
+		0.0f, 0.0f, 1.0f, 0.0f,
+		0.0f, 0.0f, 0.0f, 1.0f,
+	};
+
+	worldMatrix =
+	{
+		1.0f, 0.0f, 0.0f, 0.0f,
+		0.0f, 1.0f, 0.0f, 0.0f,
+		0.0f, 0.0f, 1.0f, 0.0f,
+		0.0f, 0.0f, 0.0f, 1.0f,
+	};
+
+}
+
+
+GameEntity::~GameEntity()
+{
+}
+
+void GameEntity::SetWorldMatrix()
+{
+}
+
+DirectX::XMFLOAT4X4 GameEntity::GetWorldMatrix()
+{
+	return DirectX::XMFLOAT4X4();
+}
+
+void GameEntity::SetTranslation(float x, float y, float z)
+{
+}
+
+void GameEntity::SetRotation(float x, float y, float z)
+{
+}
+
+void GameEntity::SetScale(float x, float y, float z)
+{
+}
+
+void GameEntity::Move(float x, float y, float z)
+{
+}
+
+void GameEntity::MoveForward(float x, float y, float z)
+{
+}
